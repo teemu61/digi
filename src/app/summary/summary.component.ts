5168cbbba2b95162a79f8622d618ab8a1bf32bd4
Teemu Jappinen <teemu.jappinen@cgi.com>
2020-08-27 10:54:09 +0300

    add simple filtering

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -5,6 +5,8 @@ import { Train } from '../model/Train';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { DataSource } from '@angular/cdk/collections';
+import { map } from 'rxjs/operators';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 
@@ -15,44 +17,26 @@ import { DataSource } from '@angular/cdk/collections';
 })
 export class SummaryComponent implements OnInit {
 
-    dataSource = new UserDataSource(this.trainService);
+    dataSource = new MatTableDataSource([]);
     displayedColumns: string[] = ["trainNumber","operatorUICCode", "operatorShortCode", "trainType"];
     trains: Train[] = [];
   
     constructor(
       public trainService: TrainService,
-      private router: Router) { }
+      private router: Router) { 
+      }
 
     ngOnInit(): void {
-      this.trainService.getTrains().subscribe((resp: any) => {
-        this.trains = resp;
-      });
-      
-    }
-  
-    getTrains(): void {
-      console.log("getTrains called again")
-      this.trainService.getTrains().subscribe((resp: any) => {
-        this.trains = resp;
-      });
-    }
-  
-    getTrainById(id: number): Train { 
-      return this.trains.filter(i => i.trainNumber == id )[0]; 
-    }
-  
-  }
-  
-    export class UserDataSource extends DataSource<any> {
-  
-      constructor(private restService: TrainService) {
-        super();
-      }
 
-      connect(): Observable<Train[]> {
-        return this.restService.getTrains();
-      }
-      
-      disconnect() {
-      }
+      this.trainService.getTrains().subscribe(
+        trains => this.dataSource.data = trains
+      );
+
+
     }
+
+    searchTrains(search = '') {
+        console.log("searchTrain called: "+search)
+        this.dataSource.filter = search.toLocaleLowerCase().trim();
+    }
+  }
